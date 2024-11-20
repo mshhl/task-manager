2b270f811ad0149f2d1c21d3031ee004c6c382ed
Mohammed Mishhal <mishhalstep2future@gmail.com>
2024-11-20 15:23:20 +0530

    signup validation has completed

--- controllers/userControllers/authenticationController.js
+++ controllers/userControllers/authenticationController.js
@@ -5,10 +5,37 @@ const loadLoginPage = function(req,res){
     res.render("userViews/login");
     
 }
+// registration logic
 const loadSignupPage = function(req,res){
     res.render("userViews/signup");
 }
+const registerUser = function(req,res){
+    console.log("hey hello register user");
+
+    const validateEmail = function(email){
+        const  regex = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/;
+        return regex.test(email)
+
+    }
+    const {fullname,email,password,cfmpassword} = req.body;
+   
+    if( fullname == ""){
+        res.render("userViews/signup",{errorMessage:"Name is not valid"});
+        return;
+    }else if(!validateEmail(email)){
+        console.log("cool email")
+        res.render("userViews/signup",{emailError:"Email is not valid"});
+        return;
+    }else if( password !== cfmpassword){
+        console.log("password not match")
+           res.render("userViews/signup",{passwordError:"password doesn't match"})
+           return;
+    }else{
+        res.redirect("/");
+    }
+}
 module.exports = {
     loadLoginPage,
-    loadSignupPage
+    loadSignupPage,
+    registerUser
 }
--- router/userRoute/authenticationRouter.js
+++ router/userRoute/authenticationRouter.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const {loadLoginPage,loadSignupPage} = require("../../controllers/userControllers/authenticationController");
+const {loadLoginPage,loadSignupPage,registerUser} = require("../../controllers/userControllers/authenticationController");
 
 
 router.route("/").get(loadLoginPage);
-router.route("/register").get(loadSignupPage);
+router.route("/register").get(loadSignupPage).post(registerUser);
 
 
 
