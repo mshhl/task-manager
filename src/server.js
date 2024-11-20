0f05b9c9bf37b1f5de010ee1363c9de5893e5f8d
Mohammed Mishhal <mishhalstep2future@gmail.com>
2024-11-20 11:24:34 +0530

    server configured 2nd time

--- src/server.js
+++ src/server.js
@@ -3,7 +3,10 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv").config();
 
-
+// we set the view engine and view directory
+app.set("views","./views")
+app.set("view engine","ejs");
+app.use(express.static('public'))
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
